53216f16cc339d5fd071aa5adea5d30c3a38b109
thugseus <32243869+thugseus@users.noreply.github.com>
2022-10-04 12:08:09 -0700

    Update debian.sh

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -110,7 +110,7 @@ _latest_version=$(basename $(cat ./_latest_release | grep -wo "NginxProxyManager
 
 # Download nginx-proxy-manager source
 log "Downloading NPM v$_latest_version"
-runcmd 'wget $WGETOPT -c $NPMURL/archive/v$_latest_version.tar.gz -O - | tar -xz'
+runcmd 'wget $WGETOPT -c $NPMURL/archive/refs/tags/v$_latest_version.tar.gz -O - | tar -xz'
 cd ./nginx-proxy-manager-$_latest_version
 
 log "Setting up enviroment"
